d6963fbaf3f70c0caf554f3911c5c33e2eea0140
hankliu5 <yliu224@ncsu.edu>
2018-06-10 16:24:16 -0400

    moved array generation out and take command line argument.

--- concurrency/mergesort/main.go
+++ concurrency/mergesort/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"reflect"
+	"strconv"
 	"time"
 )
 
-func main() {
-	size := 2097152
-
-	fmt.Println("generate numbers...")
-	s := make([]float64, size)
+func generateArray(numOfElements int) []float64 {
+	s := make([]float64, numOfElements)
 	for i := 0; i < cap(s); i++ {
-		s[i] = rand.Float64() * float64(size)
+		s[i] = rand.Float64() * float64(numOfElements)
 	}
+	return s
+}
+
+func main() {
+	var size int
+	if len(os.Args) < 2 {
+		size = 2097152
+	} else {
+		size, _ = strconv.Atoi(os.Args[1])
+	}
+
+	fmt.Println("generate " + strconv.Itoa(size) + " numbers...")
+	s := generateArray(size)
 
 	fmt.Println("running multithread without limited number of threads")
 	start := time.Now()
