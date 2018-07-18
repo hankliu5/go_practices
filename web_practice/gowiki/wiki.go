5dd0b78dd34486817f1ba57588307e522c7289ad
hankliu5 <yliu224@ncsu.edu>
2018-07-17 21:58:06 -0400

    added a viewHandler.

--- web_practice/gowiki/wiki.go
+++ web_practice/gowiki/wiki.go
@@ -30,7 +30,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
