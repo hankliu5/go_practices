5ad43665dc595b994a28e46c4ba4f626c5068d2d
hankliu5 <yliu224@ncsu.edu>
2018-07-31 22:42:59 -0400

    allows tcp connection and separate html.

--- web_socket/main.go
+++ web_socket/main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"io"
 	"log"
+	"net"
 	"net/http"
 
 	"golang.org/x/net/websocket"
@@ -13,19 +14,7 @@ import (
 const listenAddr = "localhost:4000"
 
 var partner = make(chan io.ReadWriteCloser)
-var rootTemplate = template.Must(template.New("root").Parse(
-	`
-	<!DOCTYPE html>
-	<html>
-	<head>
-	<meta charset="utf-8" />
-	<script>
-		websocket = new WebSocket("ws://{{.}}/socket");
-		websocket.onmessage = function(m) { console.log("Received:", m.data); };
-	</script>
-	</head>
-	</html>
-	`))
+var rootTemplate = template.Must(template.ParseFiles("index.html"))
 
 type socket struct {
 	io.ReadWriter
@@ -38,11 +27,26 @@ func (s socket) Close() error {
 }
 
 func main() {
+	go netListen()
 	http.HandleFunc("/", rootHandler)
 	http.Handle("/socket", websocket.Handler(socketHandler))
 	err := http.ListenAndServe(listenAddr, nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func netListen() {
+	l, err := net.Listen("tcp", "localhost:4001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go match(c)
 	}
 }
 
