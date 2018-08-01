7488410aa82972f23c43815a812ac06956d7a315
hankliu5 <yliu224@ncsu.edu>
2018-07-31 21:14:29 -0400

    finished fake search website.

---
+++ web_search/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/hankliu5/go_practices/concurrency/fake_search/search"
+)
+
+var listenAddr = "localhost:4000"
+var responseTemplate = template.Must(template.New("results").Parse(`
+<html>
+<head/>
+<body>
+<ol>
+{{range .Results}}
+<li>{{.Title}} - <a href="{{.URL}}">{{.URL}}</a></li>
+{{end}}
+</ol>
+<p>{{len .Results}} results in {{.Elapsed}}</p>
+</body>
+</html>
+`))
+
+type response struct {
+	Results []search.Result
+	Elapsed time.Duration
+}
+
+func main() {
+	http.HandleFunc("/search", handleSearch)
+	fmt.Println("Serving on http://localhost:4000/search")
+	err := http.ListenAndServe(listenAddr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleSearch(w http.ResponseWriter, req *http.Request) {
+	log.Println("serving", req.URL)
+	query := req.FormValue("q")
+	if query == "" {
+		http.Error(w, `missing "q" URL parameter`, http.StatusBadRequest)
+		return
+	}
+	start := time.Now()
+	timeout := 100 * time.Millisecond
+	results, err := search.Replicated(query, timeout)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	elapsed := time.Since(start)
+	resp := response{results, elapsed}
+	switch req.FormValue("output") {
+	case "json":
+		err := json.NewEncoder(w).Encode(resp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "prettyjson":
+		var b []byte
+		b, err := json.MarshalIndent(resp, "", "  ")
+		if err == nil {
+			_, err = w.Write(b)
+		}
+	default:
+		err := responseTemplate.Execute(w, resp)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
